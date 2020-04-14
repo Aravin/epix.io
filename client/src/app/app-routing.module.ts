b217dff15692c45d68fa5a58ce3c80c2b7bf4672
Aravind Appadurai <aravin.it@gmail.com>
2020-04-14 19:46:20 +0530

    Added generate-password

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HashComponent } from './features/crypto/hash/hash.component';
+import { PasswordGeneratorComponent } from './features/crypto/password-generator/password-generator.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,9 @@ const routes: Routes = [
   },
   {
     path: 'crypto/hash', component: HashComponent, data: { title: 'Hash' }
+  },
+  {
+    path: 'crypto/password-generator', component: PasswordGeneratorComponent, data: { title: 'Password Generator' }
   },
   {
     path: '**', component: HomeComponent,
