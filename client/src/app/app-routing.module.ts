2920f402988c99df0e47d4536aaa29f5321ca8fd
Aravind Appadurai <aravin.it@gmail.com>
2020-04-25 21:37:11 +0530

    Updated Homepage and .section, .container to each page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
     path: '', redirectTo: 'home', pathMatch: 'full'
   },
   {
-    path: 'home', component: MobileAppsComponent, data: { title: 'Home' }
+    path: 'home', component: HomeComponent, data: { title: 'Home' }
   },
   {
     path: 'crypto/hash', component: HashComponent, data: { title: 'Hash' }
