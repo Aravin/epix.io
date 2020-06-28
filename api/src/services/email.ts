f4810e171ee13fab5dd6c0bbedce8271005946ad
Aravind Appadurai <aravin.it@gmail.com>
2020-06-28 14:51:33 +0530

    Updated fireabse config and formatted email API

--- api/src/services/email.ts
+++ api/src/services/email.ts
@@ -1,38 +1,50 @@
 import { MailService, MailDataRequired } from '@sendgrid/mail';
 import koa from 'koa';
 
-export async function sendEmail(ctx: koa.Context) {
-    
-    const mailService = new MailService();
+export async function sendEmail(ctx: koa.Context)
+{
+    try
+    {
+        const mailService = new MailService();
 
-    mailService.setApiKey(process.env.SENDGRID_API_KEY as string);
+        mailService.setApiKey(process.env.SENDGRID_API_KEY as string);
 
-    const body = ctx.request.body;
+        const body = ctx.request.body;
 
-    const msg: MailDataRequired = {
-        to: body.to,
-        cc: body.cc,
-        bcc: body.bcc,
-        from: body.from, // Use the email address or domain you verified above
-        replyTo: body.replyTo,
-        subject: body.subject,
-        text: body.text,
-        html: body.html,
-    };
+        const msg: MailDataRequired = {
+            to: body.to,
+            cc: body.cc,
+            bcc: body.bcc,
+            from: body.from, // Use the email address or domain you verified above
+            replyTo: body.replyTo,
+            subject: body.subject,
+            text: body.text,
+            html: body.html,
+        };
 
-    //ES6
-    const status = mailService
-        .send(msg)
-        .then(success => { return true },
-            error => {  return false 
-        })
-        .catch(err => {return false});
+        //ES6
+        const status = mailService
+            .send(msg)
+            .then(success => { return true },
+                error => {  return false 
+            })
+            .catch(err => {return false});
 
-    if (status) {
-        ctx.status = 200;
-        ctx.body = 'OK'
-    } else {
-        ctx.status = 400,
-        ctx.body = 'Failed'
+        if (status)
+        {
+            ctx.status = 200;
+            return;
+        }
+        else
+        {
+            ctx.status = 400;
+            return;
+        }
+    }
+    catch (err)
+    {
+        ctx.response.body = err;
+        ctx.status = 500;
+        return;
     }
 }
