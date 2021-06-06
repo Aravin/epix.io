d66286b59078c7c11936606afdb2b11dc0816229
Aravind Appadurai <aravin.it@gmail.com>
2021-06-06 21:11:15 +0530

    Added Basic SIP calculator

--- next/pages/calculators/sip-calculator.tsx
+++ next/pages/calculators/sip-calculator.tsx
@@ -1,58 +1,98 @@
 import Layout from "../../component/layout/layout";
-import React, { useState } from "react";
-import { useForm, SubmitHandler, ChangeHandler } from "react-hook-form";
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { Bar, Doughnut } from "react-chartjs-2";
 
-let amountInvested = 0;
-let sipValue = 0;
-let wealthGained = 0;
-let overallReturnPercent = 0;
-let overallReturnAbs = 0;
-const numFormat = new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'INR' });
+const numFormat = new Intl.NumberFormat("en-IN", {
+  style: "currency",
+  currency: "INR",
+});
 
+let chartData = {};
+
+const options = {
+  responsive: false,
+  maintainAspectRatio: false,
+  legend: { display: false },
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  },
+};
 
 export default function SipCalculator() {
   const {
     register,
-    handleSubmit,
     watch,
     formState: { errors },
-    setValue 
-  } = useForm();
+  } = useForm({
+    defaultValues: {
+      amount: 5000,
+      returns: 12,
+      period: 10,
+      frequency: "12",
+    },
+  });
   // const onSubmit = (data: any) => console.log(data);
-  const onChange = (data: any) => {
-   
-    const amount = watch("amount");
-    const returns = watch("returns") / 12 / 100;
-    const tenure = watch("period");
-    const frequency = watch("frequency");
 
-    sipValue =
+  let data = watch();
+
+  const [amountInvested, setAmountInvested] = useState(0);
+  const [sipValue, setSipValue] = useState(0);
+  const [wealthGained, setWealthGained] = useState(0);
+  const [overallReturnPercent, setOverallReturnPercent] = useState(0);
+  const [overallReturnsAbs, setOverallReturnAbs] = useState(0);
+
+  useEffect(() => {
+    const amount = data.amount;
+    const tenure = data.period;
+    const frequency = parseInt(data.frequency, 10) || 12;
+    const returns = data.returns / 12 / 100;
+
+    let sipValue =
       amount *
       ((Math.pow(1 + returns, frequency * tenure) - 1) / returns) *
       (1 + returns);
-    sipValue = parseFloat(sipValue.toFixed(2));
-    amountInvested = amount * tenure * frequency;
-    wealthGained = sipValue - amountInvested;
-    overallReturnAbs = ((amountInvested - sipValue) * -1) / amountInvested;
-    overallReturnPercent = parseFloat((overallReturnAbs * 100).toFixed(2));
+    setSipValue(parseFloat(sipValue.toFixed(2)));
+    setAmountInvested(amount * tenure * frequency);
+    setWealthGained(sipValue - amountInvested);
+    setOverallReturnAbs(((amountInvested - sipValue) * -1) / amountInvested);
+    setOverallReturnPercent(parseFloat((overallReturnsAbs * 100).toFixed(2)));
 
     console.log({ amount, returns, tenure, frequency });
     console.log({ sipValue, amountInvested, wealthGained });
-  };
-  
+
+    chartData = {
+      labels: ["Investment", "Returns"],
+
+      datasets: [
+        {
+          label: "",
+          data: [amountInvested, sipValue],
+          backgroundColor: ["rgba(209, 213, 219)", "rgba(110, 231, 183)"],
+          hoverOffset: 4,
+        },
+      ],
+    };
+  }, [data]);
+
   console.log({ errors });
 
   return (
     <Layout title="SIP Calculator" subTitle="Systematic Investment Plan">
-      <div className="flex flex-row gap-x-5">
-        <div className="flex-1 shadow-md drop-shadow-md p-4">
+      <div className="flex flex-row flex-wrap gap-x-5">
+        <div className="flex-1 shadow-md drop-shadow-md p-4 sm:p-2">
           <h3 className="text-xl pb-4 text-green-600"> Calculate </h3>
 
-          <form className="space-y-5" onChange={handleSubmit(onChange)}>
+          <form className="space-y-5">
             <label className="block">
               <span className="text-gray-700">Investment Amount*</span>
               <input
-                defaultValue="5000"
                 className="form-input mt-1 block w-full"
                 type="number"
                 placeholder="Investment Amount"
@@ -67,45 +107,38 @@ export default function SipCalculator() {
 
             <div className="block">
               <span className="text-gray-700">Investment Frequency*</span>
+
               <div className="mt-2 space-x-5">
                 <label className="inline-flex items-center">
                   <input
-                    className="form-radio"
+                    {...register("frequency", {
+                      required: true,
+                    })}
+                    className="form-radio ring-color-green-500"
                     type="radio"
-                    {...register("frequency", { required : true })}
                     value="12"
-                    
-                    
-                    // defaultChecked={tenureRadio ==="12"}
-                    // onClick={(e) => { e.preventDefault();setTenureRadio('12')}}
-                    // onChange={(e) => props.onChange(e.target.checked)} checked={props.value || false}
-                    // onChange={() => setValue("frequency", '12')}
                   />
                   <span className="ml-2">Monthly</span>
                 </label>
                 <label className="inline-flex items-center">
                   <input
+                    {...register("frequency", {
+                      required: true,
+                    })}
                     className="form-radio"
-                    {...register("frequency")}
                     type="radio"
                     value="4"
-                    // defaultChecked={tenureRadio === '4'}
-                    // onClick={(e) => { e.preventDefault();setTenureRadio('4')}}
-                    // onChange={(e) => props.onChange(e.target.checked)} checked={props.value || false}
-                    // onChange={() => setValue("frequency", '4')}
                   />
                   <span className="ml-2">Quarterly</span>
                 </label>
                 <label className="inline-flex items-center">
                   <input
+                    {...register("frequency", {
+                      required: true,
+                    })}
                     className="form-radio"
-                    {...register("frequency")}
                     type="radio"
                     value="1"
-                    // defaultChecked={tenureRadio === '1'}
-                    // onClick={(e) => { e.preventDefault();setTenureRadio('1')}}
-                    // onChange={(e) => props.onChange(e.target.checked)} checked={props.value || false}
-                    // onChange={() => setValue("frequency", '1')}
                   />
                   <span className="ml-2">Yearly</span>
                 </label>
@@ -115,11 +148,10 @@ export default function SipCalculator() {
             <div>
               <label className="block">
                 <span className="text-gray-700">
-                  Expected Annual Returns ({watch("returns")}%)*
+                  Expected Annual Returns ({data.returns}%)*
                 </span>
                 <input
-                  defaultValue="12"
-                  className="form-range mt-1 w-5/6"
+                  className="form-range mt-1 w-11/12"
                   type="range"
                   min="5"
                   max="40"
@@ -130,31 +162,30 @@ export default function SipCalculator() {
                     min: 5,
                   })}
                 />
-
-                <input
-                  defaultValue="12"
+                <span className="bg-green-300 rounded-full inline-flex mt-1 w-1/12 items-center justify-center">
+                  {data.returns}
+                </span>
+                {/* <input
                   className="form-input mt-1 w-1/6"
                   type="number"
                   placeholder="12%"
-                  value={watch("returns")}
-                  {...register("returnsText", {
+                  {...register("returns", {
                     required: true,
                     max: 40,
                     min: 5,
                     maxLength: 5,
                   })}
-                />
+                /> */}
               </label>
             </div>
 
             <div>
               <label className="block">
                 <span className="text-gray-700">
-                  Investment Period (in {watch("period")} Years)*
+                  Investment Period (in {data.period} Years)*
                 </span>
                 <input
-                  defaultValue="10"
-                  className="form-range mt-1 w-5/6"
+                  className="form-range mt-1 w-11/12"
                   type="range"
                   min="5"
                   max="50"
@@ -162,23 +193,25 @@ export default function SipCalculator() {
                   placeholder="10"
                   {...register("period", { required: true })}
                 />
-                <input
-                  defaultValue="10"
+                <span className="bg-green-300 rounded-full inline-flex mt-1 w-1/12 items-center justify-center ">
+                  {data.period}
+                </span>
+                {/* <input
                   className="form-input mt-1 w-1/6"
                   type="number"
-                  value={watch("period")}
-                  {...register("periodText", {
+                  {...register("period", {
                     required: true,
                     max: 50,
                     min: 0.5,
                     maxLength: 80,
                   })}
-                />
+                /> */}
               </label>
             </div>
           </form>
         </div>
-        <div className="flex-1 shadow-md drop-shadow-md p-4">
+
+        <div className="flex-1 shadow-md drop-shadow-md p-4 sm:p-2">
           <h3 className="text-xl pb-4 text-green-600"> Result </h3>
           <table className="table-auto">
             <thead>
@@ -207,29 +240,40 @@ export default function SipCalculator() {
             </tbody>
           </table>
         </div>
-      </div>
 
-      <div className="flex flex-row mt-8">
-        <div className="flex-1 p-2 shadow-md">
-          <h3 className="text-xl pb-4 text-green-600"> About SIP </h3>
-          <p>
-            Systematic Investment Plan (SIP) is an investment route offered by
-            Mutual Funds wherein one can invest a fixed amount in a Mutual Fund
-            scheme at regular intervals– say once a month or once a quarter,
-            instead of making a lump-sum investment. The installment amount
-            could be as little as INR 500 a month and is similar to a recurring
-            deposit. It’s convenient as you can give your bank standing
-            instructions to debit the amount every month. more details can be
-            found here:{" "}
-            <a
-              href="https://www.mutualfundssahihai.com/en/what-is-sip#"
-              target="_blank"
-            >
-              https://www.mutualfundssahihai.com/en/what-is-sip#
-            </a>
-          </p>
+        <div className="flex-1 shadow-md drop-shadow-md p-4 sm:p-2">
+          <h3 className="text-xl pb-4 text-green-600">
+            {" "}
+            Amount Invested vs Returns{" "}
+          </h3>
+          <div className="w-full">
+            <Doughnut className="w-full" type="" data={chartData} />
+          </div>
         </div>
       </div>
+
+      <div className="flex flex-row">
+        <div className="flex-1 shadow-md drop-shadow-md p-4 sm:p-2">
+          <h3 className="text-xl pb-4 text-green-600"> About SIP </h3>
+
+          <p className="text-justify overflow-ellipsis overflow-hidden">
+              Systematic Investment Plan (SIP) is an investment route offered by
+              Mutual Funds wherein one can invest a fixed amount in a Mutual
+              Fund scheme at regular intervals– say once a month or once a
+              quarter, instead of making a lump-sum investment. The installment
+              amount could be as little as INR 500 a month and is similar to a
+              recurring deposit. It’s convenient as you can give your bank
+              standing instructions to debit the amount every month. more
+              details can be found here: { " " }
+              <a className="overflow-ellipsis"
+                href="https://www.mutualfundssahihai.com/en/what-is-sip#"
+                target="_blank"
+              >mutualfundssahihai</a>
+            </p>
+        </div>
+      </div>
+
+
     </Layout>
   );
 }
